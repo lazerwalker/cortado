3189e4ce254bc372365be92d049d1896385d26ae
Mike Walker <michael@lazerwalker.com>
2014-11-26 18:40:20 -0800

    Add CocoaPods + ReactiveCocoa

---
+++ Podfile
@@ -0,0 +1,7 @@
+target "Cuppa" do
+  pod 'ReactiveCocoa', '~> 2.3'
+end
+
+target "Cuppa-Today" do
+  pod 'ReactiveCocoa', '~> 2.3'
+end
---
+++ Pods/Target Support Files/Pods-Cuppa/Pods-Cuppa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cuppa : NSObject
+@end
+@implementation PodsDummy_Pods_Cuppa
+@end
