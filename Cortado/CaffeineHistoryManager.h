432011ded7e388d4b93bb3f6e91fd463b815b257
Mike Walker <michael@lazerwalker.com>
2014-12-17 17:54:56 -0500

    Can fetch history

--- Cortado/CaffeineHistoryManager.h
+++ Cortado/CaffeineHistoryManager.h
@@ -1,12 +1,18 @@
 @import Foundation;
 
 @class DrinkConsumption;
+@class RACSignal;
 
 @interface CaffeineHistoryManager : NSObject
 
+@property (readonly) BOOL isAuthorized;
+
 - (void)processDrinks:(NSArray *)array;
 
+// TODO: Refactor to use RACSignal for consistency
 - (void)processDrink:(DrinkConsumption *)array
          withCompletion:(void(^)(BOOL success, NSError *error))completion;
+
+- (RACSignal *)fetchHistory;
 
 @end
--- Cortado/CaffeineHistoryManager.m
+++ Cortado/CaffeineHistoryManager.m
@@ -2,6 +2,7 @@
 
 #import <Asterism/Asterism.h>
 #import <Mantle/Mantle.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 #import "Drink.h"
 #import "DrinkConsumption.h"
@@ -12,6 +13,10 @@
 
 @property (nonatomic, strong) HKHealthStore *healthStore;
 @property (nonatomic, strong) HKQuantityType *caffeineType;
+@property (nonatomic, strong) HKUnit *mgUnit;
+
+@property (readwrite, nonatomic, assign) BOOL isAuthorized;
+
 @end
 
 @implementation CaffeineHistoryManager
@@ -20,14 +25,20 @@
     self = [super init];
     if (!self) return nil;
 
+    self.isAuthorized = false;
+
     if ([HKHealthStore isHealthDataAvailable]) {
         self.healthStore = [[HKHealthStore alloc] init];
         self.caffeineType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryCaffeine];
+        self.mgUnit = [HKUnit unitFromString:@"mg"];
 
-        if ([self.healthStore authorizationStatusForType:self.caffeineType] == HKAuthorizationStatusNotDetermined) {
+        HKAuthorizationStatus status = [self.healthStore authorizationStatusForType:self.caffeineType];
+        if (status == HKAuthorizationStatusSharingAuthorized) {
+            self.isAuthorized = YES;
+        } else if (status == HKAuthorizationStatusNotDetermined) {
             NSSet *set = [NSSet setWithObject:self.caffeineType];
             [self.healthStore requestAuthorizationToShareTypes:set readTypes:set completion:^(BOOL success, NSError *error) {
-
+                self.isAuthorized = success;
             }];
         }
     }
@@ -61,8 +72,7 @@
 
 #pragma mark -
 - (HKQuantitySample *)sampleFromDrink:(DrinkConsumption *)drink {
-    HKUnit *unit = [HKUnit unitFromString:@"mg"];
-    HKQuantity *quantity = [HKQuantity quantityWithUnit:unit doubleValue:drink.caffeine.doubleValue];
+    HKQuantity *quantity = [HKQuantity quantityWithUnit:self.mgUnit doubleValue:drink.caffeine.doubleValue];
     HKQuantityType *type = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryCaffeine];
 
     NSString *name = drink.name;
@@ -82,5 +92,42 @@
                                             endDate:drink.timestamp
                                            metadata:metadata];
 }
+
+- (RACSignal *)fetchHistory {
+    return [[[[RACObserve(self, isAuthorized) startWith:@(self.isAuthorized)]
+        ignore:@NO]
+        take:1]
+        flattenMap:^RACStream *(id value) {
+            return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+                HKSampleQuery *query = [[HKSampleQuery alloc] initWithSampleType:self.caffeineType
+                                                                       predicate:nil
+                                                                           limit:HKObjectQueryNoLimit
+                                                                 sortDescriptors:nil
+                                                                  resultsHandler:^(HKSampleQuery *query, NSArray *results, NSError *error) {
+
+                    NSArray *parsedResults = ASTMap(results, ^id(HKQuantitySample *result) {
+                        NSString *name = result.metadata[@"Name"] ?: result.metadata[HKMetadataKeyFoodType] ?: @"Unknown Beverage";
+                        NSString *subtype = result.metadata[@"Subtype"];
+                        NSNumber *caffeine = @([result.quantity doubleValueForUnit:self.mgUnit]);
+
+                        Drink *drink = [[Drink alloc] initWithName:name subtype:subtype caffeine:caffeine];
+                        return [[DrinkConsumption alloc] initWithDrink:drink
+                                                             timestamp:result.startDate];
+                    });
+
+                    for (id result in parsedResults) {
+                      [subscriber sendNext:result];
+                    }
+
+                    [subscriber sendCompleted];
+                }];
+                [self.healthStore executeQuery:query];
+
+                return (RACDisposable *)nil;
+            }];
+    }];
+    return nil;
+}
+
 
 @end
