462a55e3c37271dab97d16fdca97e301dc230fce
Mike Walker <michael@lazerwalker.com>
2015-03-15 11:56:22 -0700

    Add share sheet to settings

--- Cortado/SettingsViewController.m
+++ Cortado/SettingsViewController.m
@@ -11,4 +11,24 @@
     return [storyboard instantiateInitialViewController];
 }
 
+#pragma mark -
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if ([cell.reuseIdentifier isEqualToString:@"share"]) {
+        [self showShareSheet];
+    }
+}
+
+#pragma mark -
+- (void)showShareSheet {
+    UIActivityViewController *shareController = [[UIActivityViewController alloc] initWithActivityItems:@[@"I'm tracking my caffeine consumption using Cortado!", [NSURL URLWithString:@"http://lazerwalker.com"]] applicationActivities:nil];
+
+    shareController.excludedActivityTypes = @[UIActivityTypeCopyToPasteboard, UIActivityTypeAddToReadingList];
+
+    [self.navigationController presentViewController:shareController animated:YES completion:nil];
+}
+
 @end
