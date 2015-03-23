204f3cc705f94c85f69e36dfd77636fa35ac5736
Mike Walker <michael@lazerwalker.com>
2015-03-23 13:33:32 -0400

    Remove CLLocationManager from AppDelegate header

--- Cortado/AppDelegate.h
+++ Cortado/AppDelegate.h
@@ -1,11 +1,8 @@
 @import UIKit;
 
-@class CLLocationManager;
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (nonatomic, strong) CLLocationManager *locationManager;
 
 - (void)manuallyCheckCurrentLocation;
 
