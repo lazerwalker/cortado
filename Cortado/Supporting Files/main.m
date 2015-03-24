ad193d2d587da4d995ba2a31eed4f49650469cb6
Mike Walker <michael@lazerwalker.com>
2015-03-23 22:31:56 -0400

    Synx

--- Cortado/Supporting Files/main.m
+++ Cortado/Supporting Files/main.m
@@ -1,12 +1,3 @@
-
-//
-//  main.m
-//  Cuppa
-//
-//  Created by Michael Walker on 11/25/14.
-//  Copyright (c) 2014 Lazerwalker. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
