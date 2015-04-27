959c321544e8b243b861910b55f51dc29647eb24
Mike Walker <michael@lazerwalker.com>
2015-04-26 23:54:15 -0400

    Add test-specific app delegate
    a la http://qualitycoding.org/app-delegate-for-tests/

--- Cortado/Supporting Files/main.m
+++ Cortado/Supporting Files/main.m
@@ -1,8 +1,11 @@
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "AppDelegate.h" 
+#import "TestingAppDelegate.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        BOOL isRunningTests = NSClassFromString(@"XCTestCase") != nil;
+        Class appDelegateClass = isRunningTests ? [TestingAppDelegate class] : [AppDelegate class];
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass(appDelegateClass));
     }
 }
