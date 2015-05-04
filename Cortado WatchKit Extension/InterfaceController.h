6ca8136c156b939431c2a5f1eec43ba4cdcda4c3
Mike Walker <michael@lazerwalker.com>
2015-05-04 15:12:22 -0400

    Add Watch target

---
+++ Cortado WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Cortado WatchKit Extension
+//
+//  Created by Mike Lazer-Walker on 5/4/15.
+//  Copyright (c) 2015 Lazerwalker. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
